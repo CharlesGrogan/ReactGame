31dd981ce4bd11a7f8f5f1f85215dc0b40776c04
Elephant_Man <grogan.charlie@gmail.com>
2018-10-05 19:16:10 -0500

    components built
    - jumbotron on screen
    - pictures displaying to screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Jumbotron from "./components/jumbotron";
+import crypto from "./crypto.json";
+// import './App.css';
 
 class App extends Component {
+  state = {
+    crypto,
+    clickedItems: []
+  };
+
+  shuffleFriend = data => {
+    const id = data;
+    for (let i = 0; i < id.length; i++) {
+      // loop over array
+      const friends = Math.floor(Math.random() * id.length); // make a random number each time
+      const temp = id[i]; // store value at index i
+      id[i] = id[friends]; // swaps value at index i with random index in array
+      id[friends] = temp; // replaces value we swapped wiht the original index value
+    }
+    this.setState({ crypto: id });
+  };
+
+  guessedRight = id => {
+    if (this.state.clickedItems.includes(id)) {
+      this.setState({ crypto, clickedItems: [] });
+    } else {
+      this.shuffleFriend(this.state.crypto);
+      const guess = [...this.state.clickedItems, id];
+      this.setState({ clickedItems: guess });
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Jumbotron />{" "}
+        {this.state.crypto.map(item => (
+          <FriendCard
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            img={item.img}
+            guess={this.guessedRight}
+          />
+        ))}
       </div>
     );
   }
